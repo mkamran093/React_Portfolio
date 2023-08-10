36945b644c3799f6bf76aae42cbf2c1a5a6beda8
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2023-08-10 16:42:44 +0500

    Testimonial section completed

--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./testimonial.css";
+import Image1 from "../../assets/avatar-1.svg";
+import Image2 from "../../assets/avatar-2.svg";
+
+// Swiper Components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "John Doe",
+    subtitle: "Product designer at Dribble",
+    comment:
+      "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "John Doe",
+    subtitle: "Product designer at Dribble",
+    comment:
+      "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+  },
+];
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section className="testimonials container section">
+      <h2 className="section_title">Clients & Reviews</h2>
+
+      <Swiper
+        className="testimonials_container grid"
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, image, title, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testimonial_item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+
+              <h3 className="testimonial_title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonial;
